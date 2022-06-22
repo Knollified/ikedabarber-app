25c524153979462bb9908888c9f8283f36d1426e
Steven Castro <steve-at-573687461965>
2022-06-22 13:45:41 -0400

    Added Mapos

--- src/container/location/Location.jsx
+++ src/container/location/Location.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import './Location.css'
-
+import "./Location.css";
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { Map } from "../../components/index";
 const Location = () => {
-    return(
-        <div className="ike__location section__padding" id="location">
-            <h1>Location</h1>
-        </div>
-    )
-}
+  const [zoom, setZoom] = React.useState(3); // initial zoom
+  const [center, setCenter] = React.useState({
+    lat: 0,
+    lng: 0,
+  });
 
-export default Location
+  return (
+    <div className="ike__location section__padding" id="location">
+      <h1>Location</h1>
+      <Wrapper apiKey={process.env.REACT_APP_GMAPS_API}>
+        <Map
+          center={center}
+          zoom={zoom}
+          style={{ width: "400px", height: "400px" }}
+        />
+      </Wrapper>
+    </div>
+  );
+};
+
+export default Location;
